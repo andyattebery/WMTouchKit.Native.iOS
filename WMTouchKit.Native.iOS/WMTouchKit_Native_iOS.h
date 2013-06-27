1b3889e7dd4b21ca42b69356e0b8805f3101dbca
Andy Attebery <aattebery@CHI-AATTEBERY1.local>
2013-06-27 18:28:15 -0500

    Initial Commit

---
+++ WMTouchKit.Native.iOS/WMTouchKit_Native_iOS.h
@@ -0,0 +1,13 @@
+//
+//  WMTouchKit_Native_iOS.h
+//  WMTouchKit.Native.iOS
+//
+//  Created by Andy Attebery on 6/27/13.
+//  Copyright (c) 2013 West Monroe Partners. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMTouchKit_Native_iOS : NSObject
+
+@end
---
+++ WMTouchKit.Native.iOS/WMTouchKit_Native_iOS.m
@@ -0,0 +1,13 @@
+//
+//  WMTouchKit_Native_iOS.m
+//  WMTouchKit.Native.iOS
+//
+//  Created by Andy Attebery on 6/27/13.
+//  Copyright (c) 2013 West Monroe Partners. All rights reserved.
+//
+
+#import "WMTouchKit_Native_iOS.h"
+
+@implementation WMTouchKit_Native_iOS
+
+@end
